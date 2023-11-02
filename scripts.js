dd39e7b8758bdd13a46bafecf5677af0e4a95343
ldduck <lachlanduckbio@outlook.com>
2023-11-02 16:31:13 +1100

    1.1.3
    Further experimentation with understanding JavaScript interactions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
 //alert("hello")
 
-// Javascript testing & experimentation
+// Javascript testing & experimentation  //
 
 //document.getElementById("insert").innerHTML = "Hello Greg!";
 
 //document.getElementById("change").style.backgroundColor = "blue";
+
+let internetSec = document.querySelector("#internet")
+//internetSticky.style.backgroundColor = "lightBlue"
+
+// Changes the content of an id
+function highlight(){
+    var element = document.getElementById("highDiv");
+    element.classList.add("highlighted");
+}
+
+// Toggles between two css styles - Might be useful when combined with slide effect (if possible?)
+function toggleFunction() {
+    var element = document.getElementById("toggleButton");
+    if (element.className == "inactive") {
+        element.className = "active";
+    } else {
+        element.className = "inactive"
+    }
+}
