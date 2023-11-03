04b2b537fe49f5d14a275fe1c843bec79526491a
ldduck <lachlanduckbio@outlook.com>
2023-11-03 14:24:29 +1100

    1.3
    added CSS and JavaScript for collapsible boxes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,5 @@
 //alert("hello")
 
-// Javascript testing & experimentation  //
-
-//document.getElementById("insert").innerHTML = "Hello Greg!";
-
-//document.getElementById("change").style.backgroundColor = "blue";
-
-
-
 // Changes the content of an id
 function highlight(){
     var element = document.getElementById("highDiv");
@@ -24,6 +16,23 @@ function toggleFunction() {
     }
 }
 
+// COLLAPSIBLE BOXES //
+// declaring variables, var collapsed is setting a class as a variable
+var collapsed = document.getElementsByClassName("collapsing");
+var i;
+
+//for loop with an if else statement adding a toggle button and creating the box on a click
+for (i=0; i< collapsed.length;i++) {
+    collapsed[i].addEventListener("click", function() {
+        this.classList.toggle("open"); // this. allows this effect to be used else where, specifying this specific instance
+        var content = this.nextElementSibling; // connects the two boxes together
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        }   else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
 
 
 
